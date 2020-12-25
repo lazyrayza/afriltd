e35bccbf861c92b467dc508bc4326851e1ddb279
Raza Pirbhai razaP_13@live.com <razaP_13@live.com>
2020-12-25 17:15:43 +0300

    removed another remove_column from last migrattion to test

--- db/migrate/20200708104752_remove_columns_from_carmodels.rb
+++ db/migrate/20200708104752_remove_columns_from_carmodels.rb
@@ -3,6 +3,5 @@ class RemoveColumnsFromCarmodels < ActiveRecord::Migration[6.0]
     remove_column :carmodels, :mileage
     remove_column :carmodels, :fuel_id
     remove_column :carmodels, :year
-    remove_column :carmodels, :aircon
   end
 end
